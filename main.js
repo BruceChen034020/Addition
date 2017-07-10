e667235a96b6cc972a1ccf0c4c496d0a6c345430
BruceChen034020 <2017greatplatform@gmail.com>
2017-07-10 16:44:32 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -35,7 +35,40 @@ function draw(){
   
 }
 
+var label1;
+var textBox3;
+var button4;
+var label2;
+
 function Enter(){
   button3.innerHTML = parseInt(textBox1.value) + parseInt(textBox2.value);
+
+  createP("");
+
+  label1 = document.createElement("label");
+  document.body.appendChild(label1);
+  label1.innerHTML = "接下來請輸入圓半徑";
+
+  createP('');
+
+  textBox3 = document.createElement("textarea");
+  document.body.appendChild(textBox3);
+
+  button4 = document.createElement("button");
+  document.body.appendChild(button4);
+  button4.innerHTML = "Enter";
+  button4.addEventListener("click", Enter2);
+
+  createP('');
+
+  label2 = document.createElement("label");
+  document.body.appendChild(label2);
+  
 }
 
+function Enter2(){
+  label2.innerHTML = "圓周長 = ";
+  label2.style.fontSize = "72px";
+  label2.style.fontFamily = "DFKai-sb";
+  label2.innerHTML += parseInt(textBox3.value) * 2 * 3.14159265358979323846264338327950;
+}
